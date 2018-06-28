afb06b0eb009af98cba0f60571fcc631415b76d0
Austin Barr and Alex Bunnell <alex@>
2018-06-28 10:16:18 -0700

    Add two additiinal tests, one to ensure the GetWord method words, the other to ensure that the method to break down a string into an array works.

--- ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrabbleScore.Models;
+using System.Linq;
 
 namespace ScrabbleScore.Test
 {
@@ -7,14 +8,40 @@ namespace ScrabbleScore.Test
   public class WordTests
   {
     [TestMethod]
+    public void DisassembleWord_TurnsIntoCharArray_True()
+    {
+      //Assign
+      Word newWord = new Word("cat");
+      char[] letterArray = {'c', 'a', 't'};
+      //Act
+      char[] newArray = newWord.DisassembleWord();
+      bool expected = newArray.SequenceEqual(letterArray);
+      //Assert
+      Assert.AreEqual(expected, true);
+    }
+    [TestMethod]
+    public void GetWord_FromSettingWord_True()
+    {
+      //Assign
+      Word newWord = new Word("a");
+
+      //Act
+      string result = newWord.GetWord();
+
+      //Assert
+      Assert.AreEqual(result, "a");
+    }
+
+    [TestMethod]
     public void GetScore_ConvertsToNumber_True()
-      {
-        //Assign
-        Word newWord = new Word("a");
-        //Act
-        int score = newWord.GetFinalScore();
-        //Assert
-        Assert.AreEqual(score, 1);
-      }
+    {
+      //Assign
+      Word newWord = new Word("a");
+      //Act
+      int score = newWord.GetFinalScore();
+      //Assert
+      Assert.AreEqual(score, 1);
+    }
+
   }
 }
