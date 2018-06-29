54383e8f0a475cd9ec2a63bae3ce7b2bb254d451
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-28 18:51:17 -0700

    add a method to check if input was invalid

--- ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
+++ ScrabbleScore.Tests/ModelTests/ScrabbleScoreTests.cs
@@ -11,6 +11,11 @@ namespace ScrabbleScore.Test
     [TestMethod]
     public void DisassembleWord_TurnsIntoCharArray_True()
     {
+      //creatring a new instance of the Word object
+      //setting a char[] called letterArray
+      //newArray is a new char Array by taking the instance of word called newWor
+      //bool expected returns a boolean value by running SequenceEqual which compares two char array's
+      //we are then seeing if expected will be true.
       //Assign
       Word newWord = new Word("cat");
       char[] letterArray = {'c', 'a', 't'};
@@ -21,6 +26,7 @@ namespace ScrabbleScore.Test
       Assert.AreEqual(expected, true);
     }
     [TestMethod]
+    //checking if get word is actually retrieving the value of _userWord
     public void GetWord_FromSettingWord_True()
     {
       //Assign
@@ -34,6 +40,7 @@ namespace ScrabbleScore.Test
     }
 
     [TestMethod]
+    //checking if GetFinalScore is actually working by creating an instance and runnig it on it
     public void GetFinalScore_ConvertsToNumber_True()
     {
       //Assign
@@ -44,6 +51,7 @@ namespace ScrabbleScore.Test
       Assert.AreEqual(score, 1);
     }
     [TestMethod]
+    // testing get GetFinalScore on a new insatcnce with multiple char
     public void GetFinalScore_ConvertsMultiLetterWordToNumber_True()
     {
       //Assign
--- ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ScrabbleScore.Models
 {
   public class Word
   {
+    //Properties of the Word Object
     private string _userWord;
     private int _userScore;
+    // Dictionary that has a key and a value.
     Dictionary<char, int> scoreDictionary = new Dictionary<char, int>() {
       {'a', 1}, {'e',1}, {'i',1}, {'o',1}, {'u',1}, {'l',1}, {'n',1}, {'r',1}, {'s',1}, {'t',1},
       {'d',2}, {'g',2},
@@ -17,34 +20,47 @@ namespace ScrabbleScore.Models
       {'j',8}, {'x',8},
       {'q',10}, {'z',10},
       };
-
+      //Constructor for an object Word, which excepts an argument. Which would create an instance of the Word object
     public Word(string userWord)
     {
       _userWord = userWord;
       _userScore = 0;
     }
+    //Getter for private property, so you can use it in another method/ or program
     public string GetWord()
     {
       return _userWord;
     }
+    // setting a _userWord of the private propery of the word object
     public void SetWord(string newWord)
     {
       _userWord = newWord;
     }
+    // Getting the _userScore of the private property of the word object
     public int GetScore()
     {
       return _userScore;
     }
+    // Setting the _userScore by passing an int argument
     public void SetScore(int newScore)
     {
       _userScore = newScore;
     }
+    //unnececaery but getWord() from this which refers to the object Word
+    //we take that word and turn it into a char Array lowercase
+    //returns a lowercase char[]
     public char[] DisassembleWord()
     {
       string userWord = this.GetWord();
       char[] letterArray = userWord.ToLower().ToCharArray();
       return letterArray;
     }
+    //use method DisassembleWord and run it on the object word
+    //clarify thet theScore is 0
+    // run a foreach loop on the char[]array, to grab each char
+    // we then run for each loop on each char to find the matching char in the Dictionary, if we find a matching char, we takee the value from the key and add it to theScore
+    //after we run that we take theScore and run SetScore() to set _userScore to the appropriate value.
+    //then we get that value by running this.GetScore(), which grabs the score form the object word
     public int GetFinalScore()
     {
       char[] letterArray = this.DisassembleWord();
@@ -70,9 +86,21 @@ namespace ScrabbleScore.Models
     public static void Main()
     {
       Console.WriteLine("Enter a single word to find its unmodified scrabble value:");
-      Word newWord = new Word(Console.ReadLine());
-      // int userFinalScore = newWord.GetFinalScore();
-      Console.WriteLine("The Unmodified scrabble score of " + newWord.GetWord() + " is: " + newWord.GetFinalScore());
+      string mainInput = Console.ReadLine();
+      bool input = Regex.IsMatch(mainInput, @"^[a-zA-Z]+$");
+      if(input == true)
+      {
+        Word newWord = new Word(mainInput);
+        // unnececaery
+        // int userFinalScore = newWord.GetFinalScore();
+        // showing the users word by running GetWord()
+        // grabbing the new instacnce newWord and runnung GetFinalScore()
+        Console.WriteLine("The Unmodified scrabble score of " + newWord.GetWord() + " is: " + newWord.GetFinalScore());
+      }
+      else{
+        Console.WriteLine("Invalid: Please enter a word!!");
+        Main();
+      }
     }
   }
 }
